180b22891867d6194aecdd5769733870e65d5f9e
Quang Truong <>
2025-04-17 14:43:45 +0700

    Fix ui issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
-// Function to handle scroll event
-window.onscroll = function () {
-  var navbar = document.getElementById("navbar");
-  if (window.scrollY > 50) {
-    // When scroll position is more than 50px from top
-    navbar.classList.add("scrolled");
-  } else {
-    navbar.classList.remove("scrolled");
-  }
+const main = () => {
+  const navbar = $("#navbar");
+  const btnNavBar = $(".btn-navbar-menu");
+
+  // Function to handle scroll event
+  window.onscroll = function () {
+    if (window.scrollY > 50) {
+      // When scroll position is more than 50px from top
+      navbar.addClass("scrolled");
+    } else {
+      navbar.removeClass("scrolled");
+    }
+  };
+
+  btnNavBar.on("click", () => {
+    navbar.toggleClass("showing");
+  });
 };
+
+$(document).ready(() => {
+  main();
+});
